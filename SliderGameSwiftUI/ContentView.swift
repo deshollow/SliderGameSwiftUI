52d06d5f1abf0d7665b758ee3f74c95dfd789911
deshollow <555demin@gmail.com>
2023-12-24 19:21:53 +0300

    done

--- SliderGameSwiftUI/ContentView.swift
+++ SliderGameSwiftUI/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var targetValue = Int.random(in: 1..<100)
+    @State var showAlert = false
+    @State var currentValue = 0
+    
+    @State private var buttonCounter = 0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Подвинь слайдер, как можно ближе к: \(targetValue)")
+            HStack{
+                Text("0")
+                UISliderViewRepresentation(value: $currentValue)
+                
+                Text("100")
+            }.padding()
+            VStack{
+                Button("Проверь меня", systemImage: "person.fill.checkmark") {
+                    self.showAlert = true
+                    
+                } .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Твой результат"),
+                          message: Text("\(computeScore())"),
+                          dismissButton: .default(Text("Ok")))
+                }
+                .font(.title2)
+                .padding()
+                Button("Начать заново", systemImage: "trash") {
+                    targetValue = Int.random(in: 1..<100)
+                    currentValue = 0
+                    buttonCounter += 1
+                }
+                .symbolEffect(.bounce, value: buttonCounter)
+                .font(.title2)
+            }
         }
         .padding()
+    }
+    private func computeScore() -> Int {
+        var result = 0
+        let difference = abs(targetValue - lround(Double(currentValue)))
+        let temp = 100 - difference
+        result = temp
+        return result
     }
 }
 
---
+++ SliderGameSwiftUI/UISliderViewRepresentation.swift
@@ -0,0 +1,56 @@
+//
+//  UISliderViewRepresentation.swift
+//  SliderGameSwiftUI
+//
+//  Created by deshollow on 24.12.2023.
+//
+
+import SwiftUI
+
+struct UISliderViewRepresentation: UIViewRepresentable {
+    
+    @Binding var value: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+    
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        
+        slider.addTarget(context.coordinator,
+                         action:#selector(Coordinator.didChooseValue),
+                         for: .valueChanged
+        )
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(value)
+    }
+    
+    //typealias UIViewType = UISlider
+}
+
+extension UISliderViewRepresentation {
+    class Coordinator: NSObject {
+        @Binding var value: Int
+        
+        init(value: Binding<Int>) {
+            self._value = value
+        }
+        
+        @objc func didChooseValue(_ sender: UISlider) {
+            value = Int(sender.value)
+        }
+    }
+}
+
+#Preview {
+    UISliderViewRepresentation(value: .constant(50))
+}
